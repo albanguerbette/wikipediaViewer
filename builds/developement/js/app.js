d1422190312d443c34360d728619eb4b0c62aea0
albangu <aguerbette@gmail.com>
2016-09-08 17:45:30 +0200

    Added Mustache then modified app.js index.html

--- builds/developement/js/app.js
+++ builds/developement/js/app.js
@@ -1,27 +1,22 @@
 $( document ).ready( function() {
 
 	var req = function( textSearched ) {
+
 		var api = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srlimit=10&srprop=snippet&srsearch=';
 		var apiReq = api + textSearched + '&callback=?';
+
 		$.getJSON(apiReq, function( data ) {
-			setHtml( data );
+
+			var template = $('#responsetpl').html();
+			var html = Mustache.to_html(template, data);
+			$('.app').html(html);
 		});
+
 	};
 
-	var setHtml = function ( data ) {
-		var html = '';
-		for ( var i = 0; i < 10; i++ ) {
-			html += '<li><h1 class="uppercase">' + data.query.search[i].title + '</h1>';
-			html += '<p>' + data.query.search[i].snippet + '</p></li>';
-		}
-		$('ul').append(html);
-	};
-
-
-	$( '#query' ).on( 'input', function(e) {
+	$( '#textSearched' ).on( 'input', function(e) {
 		e.preventDefault();
-		$( 'ul' ).html( '' );
-		var inputText = $( '#query' ).val();
+		var inputText = $( '#textSearched' ).val();
 		req( inputText );
 	});
 
