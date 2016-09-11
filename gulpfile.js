100d118555abcb3a7b22e7702eeee42b2d988212
albangu <aguerbette@gmail.com>
2016-09-11 12:55:44 +0200

    removed irrelevant directory and modified gulpfile in accordance

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,39 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	browserSync = require('browser-sync').create(),
-	postcss = require('gulp-postcss'),
-	autoprefixer = require('autoprefixer'),
-	cssnano = require('cssnano');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var browserSync = require('browser-sync').create()
+var postcss = require('gulp-postcss')
+var autoprefixer = require('autoprefixer')
+var cssnano = require('cssnano')
 
-var sassSource = ['components/sass/**/*.scss'];
-var outputDir = ['builds/developement/'];
+var sassSource = ['components/sass/']
+var jsSource = ['components/scripts/']
+var outputDir = ['builds/development/']
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass', 'js'], function () {
-	browserSync.init({
-		server: outputDir
-	});
+  browserSync.init({
+    server: './'
+  })
 
-	gulp.watch(sassSource, ['sass']);
-	gulp.watch('builds/developement/index.html').on('change', browserSync.reload);
-});
+  gulp.watch(sassSource + '*.scss', ['sass'])
+  gulp.watch('index.html').on('change', browserSync.reload)
+  gulp.watch(jsSource + '*.js', ['js']).on('change', browserSync.reload)
+})
 
 // Compile sass into CSS then Auto prefixe & auto-inject into browsers
 gulp.task('sass', function () {
-	return gulp.src(sassSource)
-		.pipe(sass().on('error', sass.logError))
-    .pipe(postcss([autoprefixer({ browsers: ['> 1%', 'last 10 versions'] }),cssnano()]))
-		.pipe(gulp.dest(outputDir + 'css'))
-		.pipe(browserSync.stream());
-});
+  return gulp.src(sassSource + '*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer({ browsers: ['> 1%', 'last 10 versions'] })]))
+    .pipe(gulp.dest(outputDir + 'css'))
+    .pipe(postcss([cssnano()]))
+    .pipe(gulp.dest('builds/production/css'))
+    .pipe(browserSync.stream())
+})
 
-gulp.task('js', function() {
-	gulp.src('components/scripts/app.js')
-	.pipe(gulp.dest(outputDir + 'js'));
-});
+gulp.task('js', function () {
+  gulp.src(jsSource + 'app.js')
+    .pipe(gulp.dest(outputDir + 'js'))
+})
 
-
-gulp.task('default', ['serve']);
+gulp.task('default', ['serve'])
