53b0dea27599619eef045e4f9ca0e4c1f50acb2d
Alban Guerbette <aguerbette@gmail.com>
2017-05-30 22:50:18 +0200

    es5 => es6

--- components/scripts/app.js
+++ components/scripts/app.js
@@ -1,29 +1,26 @@
-$(document).ready(function () {
-  var req = function (textSearched) {
-    var api = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srlimit=12&srprop=snippet&srsearch='
-    var apiReq = api + textSearched + '&callback=?'
+$(document).ready(() => {
+  const req = function req(textSearched) {
+    const api =
+      'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srlimit=12&srprop=snippet&srsearch=';
+    const apiReq = `${api + textSearched}&callback=?`;
 
-    $.getJSON(apiReq, function (data) {
-      var template = $('#responsetpl').html()
-      var html = Mustache.to_html(template, data)
-      $('.js-app').html(html)
+    $.getJSON(apiReq, (data) => {
+      const template = $('#responsetpl').html();
+      const html = Mustache.to_html(template, data);
+      $('.js-app').html(html);
       $('.content__item').children('.item__title').wrap(function () {
-        return '<a href="https://en.wikipedia.org/wiki/' + $(this).text().replace(/\s/g, '_') + '"></a>'
-      })
-    })
-  }
+        return `<a href="https://en.wikipedia.org/wiki/${$(this)
+          .text()
+          .replace(/\s/g, '_')}"></a>`;
+      });
+    });
+  };
 
-  var myInput = $('#textSearched')
+  const myInput = $('#textSearched');
   // Use Observable with debounceTime to avoid querying the api on each key stroke
-  var obs = Rx.Observable.fromEvent(myInput, 'keyup')
+  const obs = Rx.Observable.fromEvent(myInput, 'keyup');
 
-  obs
-  .debounceTime(500)
-  .map(function (event) {
-    return event.target.value
-  })
-  .subscribe(function (value) {
-    console.log(value)
-    req(value)
-  })
-})
+  obs.debounceTime(500).map(event => event.target.value).subscribe((value) => {
+    req(value);
+  });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,39 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var browserSync = require('browser-sync').create()
-var postcss = require('gulp-postcss')
-var autoprefixer = require('autoprefixer')
-var cssnano = require('cssnano')
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
-var sassSource = ['components/sass/']
-var jsSource = ['components/scripts/']
-var outputDir = ['builds/development/']
+const sassSource = ['components/sass/'];
+const jsSource = ['components/scripts/'];
+const outputDir = ['builds/development/'];
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'js'], function () {
+gulp.task('serve', ['sass', 'js'], () => {
   browserSync.init({
     server: './'
-  })
+  });
 
-  gulp.watch(sassSource + '*.scss', ['sass'])
-  gulp.watch('index.html').on('change', browserSync.reload)
-  gulp.watch(jsSource + '*.js', ['js']).on('change', browserSync.reload)
-})
+  gulp.watch(`${sassSource}*.scss`, ['sass']);
+  gulp.watch('index.html').on('change', browserSync.reload);
+  gulp.watch(`${jsSource}*.js`, ['js']).on('change', browserSync.reload);
+});
 
 // Compile sass into CSS then Auto prefixe & auto-inject into browsers
-gulp.task('sass', function () {
-  return gulp.src(sassSource + '*.scss')
+gulp.task('sass', () =>
+  gulp
+    .src(`${sassSource}*.scss`)
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([autoprefixer({ browsers: ['> 1%', 'last 10 versions'] })]))
-    .pipe(gulp.dest(outputDir + 'css'))
+    .pipe(gulp.dest(`${outputDir}css`))
     .pipe(postcss([cssnano()]))
     .pipe(gulp.dest('builds/production/css'))
     .pipe(browserSync.stream())
-})
+);
 
-gulp.task('js', function () {
-  gulp.src(jsSource + '*.js')
-    .pipe(gulp.dest(outputDir + 'js'))
-})
+gulp.task('js', () => {
+  gulp.src(`${jsSource}*.js`).pipe(gulp.dest(`${outputDir}js`));
+});
 
-gulp.task('default', ['serve'])
+gulp.task('default', ['serve']);
